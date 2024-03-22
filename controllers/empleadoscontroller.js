6ab504d57ac13231619ace689f5317ce9ee288eb
ElvisAlas <elvisalasecheverry@gmail.com>
2024-03-22 14:11:25 -0600

    correccion en el post de la imgen

--- controllers/empleadoscontroller.js
+++ controllers/empleadoscontroller.js
@@ -1,32 +1,76 @@
 import { db } from "../db/conn.js";
 
 const getEmpleado = async (req, res) => {
-    const sql = `select id, identidad, nombre, encode(foto, 'base64') AS foto from tbl_RRHH order by id`;
-    const result = await db.query(sql);
-    res.json(result)
-
-}
-
-const postEmpleado = async (req, res) => {
     try {
-        const { identidad, nombre } = req.body;
-        let foto;
+        const sql = `SELECT id, identidad, nombre, encode(foto, 'base64') AS foto FROM tbl_rrhh ORDER BY id`;
+        const result = await db.query(sql);
 
-        if (req.body.imagen!='') {
-            foto = req.body.imagen; 
-        }
-
-        const params = [identidad, nombre, foto];
-              
-        const sql = `insert into tbl_rrhh (identidad, nombre, foto) values ($1, $2, $3) returning identidad, 'Insercion Exitosa' mensaje`;
-        const result = await db.query(sql, params);
-    
         res.json(result);
     } catch (error) {
         res.status(500).json({ mensaje: error.message });
     }
 }
 
+// const postEmpleado = async (req, res) => {
+//     try {
+//         const { identidad, nombre, imagen } = req.body;
+
+//         const params = [identidad, nombre, imagen];
+//               console.log(params)
+//         const sql = `INSERT INTO tbl_rrhh (identidad, nombre, foto) VALUES ($1, $2, $3) RETURNING identidad, 'Insercion Exitosa' mensaje`;
+//         const result = await db.query(sql, params);
+    
+//         res.json(result);
+//     } catch (error) {
+//         res.status(500).json({ mensaje: error.message });
+//     }
+// }
+
+
+// const postEmpleado = async (req, res) => {
+//     try {
+//         const { identidad, nombre } = req.body;
+//         let foto;
+
+//         if (req.body.imagen!='') {
+//             foto = req.body.imagen; 
+//         }
+
+//         const params = [identidad, nombre, foto];
+              
+//         const sql = `insert into tbl_rrhh (identidad, nombre, foto) values ($1, $2, $3) returning identidad, 'Insercion Exitosa' mensaje`;
+//         const result = await db.query(sql, params);
+    
+//         res.json(result);
+//     } catch (error) {
+//         res.status(500).json({ mensaje: error.message });
+//     }
+// }
+const postEmpleado = async (req, res) => {
+
+    try{
+     
+        const { identidad, nombre } = req.body;
+     
+        const {
+            buffer     
+        } = req.file;
+    
+        const params = [identidad, nombre,buffer];
+    console.log(req.body)
+        const sql = `insert into tbl_rrhh
+                    (identidad, nombre, foto)
+                    values
+                    ($1,$2,$3) returning * , 'Insercion Exitosa' mensaje `
+    
+        const result = await db.query(sql, params);
+        res.json(result);
+    }catch{
+        res.status(500).json({ mensaje: err.message });
+    }
+
+   
+}
 
 const putEmpledo = async (req, res) => {
 
